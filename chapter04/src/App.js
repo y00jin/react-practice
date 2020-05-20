2208454801a5574336004b48b417536d46a3bf51
y00jin <59616862+y00jin@users.noreply.github.com>
2020-05-20 14:29:04 +0900

    chapter04

---
+++ chapter04/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import EventPractice from './EventPractice';
+// import Say from './Say';
+
+const App = () => {
+  // return <Counter />;
+  // return <Say />;
+  return <EventPractice />;
+};
+
+export default App;
---
+++ chapter04/src/Counter_3_4_1_3.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+
+class Counter extends Component {
+    
+    state = {
+        number: 0,
+        fixedNumber: 0
+    };
+
+    render() {
+        const {number, fixedNumber} = this.state;
+        return(
+            <div>
+                <h1>{number}</h1>
+                <h2>바뀌지 않는 값: {fixedNumber}</h2>
+                <button onClick={()=> {
+                    this.setState(prevState=>{
+                        return {
+                            number: prevState.number + 1
+                        };
+                    });
+                    // 위와 동일한 코드
+                    this.setState(prevState => ({
+                        number: prevState.number + 1
+                    }));
+                }}
+                >
+                    +1
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
